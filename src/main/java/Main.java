5af118109f82fbc33648f93951b9967374919c4c
koszar91 <72392522+koszar91@users.noreply.github.com>
2021-04-29 11:10:39 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\n----Start-----\n");
+
+
+
+        System.out.println("\n-----Exit-----\n");
+    }
+}
