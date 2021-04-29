c532f25a1fc57fdfcab888768670cd50dd83e394
koszar91 <72392522+koszar91@users.noreply.github.com>
2021-04-29 11:18:33 +0200

    adding gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n----Start-----\n");
-
+        // TODO:
 
 
         System.out.println("\n-----Exit-----\n");
